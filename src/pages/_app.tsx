da7082f6330972017f56a33db1cbe4f1669fd650
Zeke Abshire <zekeabshire@gmail.com>
2023-02-15 23:55:41 -0600

    Set up font + color palette

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,36 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { Murecho } from "@next/font/google";
+import Head from "next/head";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+
+const murecho = Murecho({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+  variable: "--font-murecho",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Parallel</title>
+        <meta name="description" content="============" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={`${murecho.variable} font-sans`}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </div>
+    </>
   );
 };
 
