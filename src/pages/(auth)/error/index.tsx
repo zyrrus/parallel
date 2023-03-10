da7e5c924ac4c2fde68b94d8854b485822fbf851
Zeke Abshire <zekeabshire@gmail.com>
2023-03-09 22:54:22 -0600

    resolved build errors

---
+++ src/pages/(auth)/error/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "@components/layouts/Layout";
+import { type NextPage } from "next";
+
+const Error: NextPage = () => {
+  return <Layout layout="info">Error</Layout>;
+};
+
+export default Error;
---
+++ src/pages/(auth)/verify-request/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "@components/layouts/Layout";
+import { type NextPage } from "next";
+
+const VerifyRequest: NextPage = () => {
+  return <Layout layout="info">Verify Request</Layout>;
+};
+
+export default VerifyRequest;
