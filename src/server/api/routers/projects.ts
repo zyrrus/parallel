2b9b81b84516592369a4f7244c4225d1097af749
Zeke Abshire <zekeabshire@gmail.com>
2023-03-24 00:12:03 -0500

    added query limit

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -3,5 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const projectsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => ctx.prisma.project.findMany()),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.project.findMany({ take: 100 });
+  }),
 });
